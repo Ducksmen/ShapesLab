ea757d6431ea7175e5c06e85378fe9fde0c3f4ef
dego <dcordova7146@bths.edu>
2018-10-01 10:18:21 -0400

    triangle finished not tested

---
+++ src/ETriangle.java
@@ -0,0 +1,16 @@
+public class ETriangle extends Shape{
+    private int side;
+
+    public ETriangle (int side){
+        super("ETriangle");
+        this.side = side;
+    }
+    @Override
+    public double getArea(){
+        return (Math.sqrt(3)/4)*(side * side);
+    }
+    @Override
+    public double getPerimeter(){
+        return 3* side;
+    }
+}
